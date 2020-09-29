c18dd6f23c2755bf0dd87a8a7cb30f4398a5a962
Jesus Minjares <60948298+jminjares4@users.noreply.github.com>
2020-09-28 18:10:34 -0600

    Update minjares_jesus_assignment_1.c

--- minjares_jesus_assignment_1.c
+++ minjares_jesus_assignment_1.c
@@ -1,16 +1,16 @@
 
 /*Author:   Jesus Minjares 
- *Assignment 01
-    Write a program that creates three processes to execute the 
-    following three programs with command line arguments:
-    1. “cat /proc/cpuinfo”
-    2. “echo Hello World”
-    3. “uname -a”
-    and then waits for these three processes to complete before creating a process to execute
-    “ls -l“ and immediately printing “Goodbye!” and exiting; not waiting for the last process to
-    complete.
-    -----------------------------------------
-    excelp(command, command, arguments, NULL)
+ Assignment 1:
+                Write a program that creates three processes to execute the 
+                following three programs with command line arguments:
+                1. “cat /proc/cpuinfo”
+                2. “echo Hello World”
+                3. “uname -a”
+                and then waits for these three processes to complete before creating a process to execute
+                “ls -l“ and immediately printing “Goodbye!” and exiting; not waiting for the last process to
+                complete.
+                -----------------------------------------
+                excelp(command, command, arguments, NULL)
  */
 #include <sys/wait.h>
 #include <sys/types.h> //fork()
